5f7eae175faa96f22bf3b541f394257feadf1d6c
HamzaChbit <hamzachbit@gmail.com>
2024-01-01 17:40:00 +0100

    navbar err

--- src/app/_components/Header/Nav/index.tsx
+++ src/app/_components/Header/Nav/index.tsx
@@ -1,18 +1,18 @@
 'use client'
-import React from 'react';
-import Link from 'next/link';
+import React from 'react'
+import Link from 'next/link'
 
-import { Header as HeaderType } from '../../../../payload/payload-types';
-import { useAuth } from '../../../_providers/Auth';
-import { Button } from '../../Button';
-import { CartLink } from '../../CartLink';
-import { CMSLink } from '../../Link';
+import { Header as HeaderType } from '../../../../payload/payload-types'
+import { useAuth } from '../../../_providers/Auth'
+import { Button } from '../../Button'
+import { CartLink } from '../../CartLink'
+import { CMSLink } from '../../Link'
 
-import classes from './index.module.scss';
+import classes from './index.module.scss'
 
 export const HeaderNav: React.FC<{ header: HeaderType }> = ({ header }) => {
-  const navItems = header?.navItems || [];
-  const { user } = useAuth();
+  const navItems = header?.navItems || []
+  const { user } = useAuth()
 
   return (
     <nav className={[classes.nav, user === undefined && classes.hide].filter(Boolean).join(' ')}>
